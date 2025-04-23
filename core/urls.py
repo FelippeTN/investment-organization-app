6e006ba46772c48063abdc8bcc64290bc58ad358
FelippeTN <felippenalim2004@gmail.com>
2025-04-23 19:20:41 -0300

    STYLE: Stylized login and registration form

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
     path('register/', register, name='register'),
 ]
