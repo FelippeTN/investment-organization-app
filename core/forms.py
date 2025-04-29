199dab9dfaec89849e8b697a11b2c69f9a620948
Wallace Barbosa <104037331+WallaceMuylaert@users.noreply.github.com>
2025-04-29 09:50:12 -0300

    Inssue/#1
    * 🧹 chore: general cleanup and structure organization
    
    * STYLE: Make the screen responsive and adjust styles, including the top bar in "operation"
    
    * CHORE: Conflito com outra top BAR no responsivo
    
    * FIX: COrreção na top bar
    
    ---------
    
    Co-authored-by: barbosasw <babosasw@pge.rj.gov.br>

--- core/forms.py
+++ core/forms.py
@@ -7,9 +7,15 @@ class OperationForm(forms.ModelForm):
         model = Operation
         fields = ['asset', 'date', 'type', 'quantity', 'unitary_price', 'brokerage']
         widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'}),
+            'date': forms.DateInput(attrs={'type': 'date', 'class': 'bg-gray-700 text-white border border-gray-600 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-emerald-400'}),
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'bg-gray-700 text-white border border-gray-600 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-emerald-400',
+            })
+            
         self.fields['asset'].queryset = Asset.objects.all()
