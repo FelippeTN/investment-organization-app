de7c99ce378583cef2d771a011abe02d17d6ef48
FelippeTN <felippenalim2004@gmail.com>
2025-04-29 10:03:47 -0300

    FIX: DATABASE CONFIG

--- investiment_organization/settings.py
+++ investiment_organization/settings.py
@@ -75,22 +75,14 @@ WSGI_APPLICATION = 'investiment_organization.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'invest_db',
-#         'USER': 'postgres',
-#         'PASSWORD': '1234',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-# Config RAIZ. Só para fortes 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'invest_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
