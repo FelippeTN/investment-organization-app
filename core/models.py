ea302df336f331c335142e66ba4e84b24a2e4bd1
FelippeTN <felippenalim2004@gmail.com>
2025-05-06 15:50:52 -0300

    FEAT: New assets informations

--- core/models.py
+++ core/models.py
@@ -6,6 +6,9 @@ class Asset(models.Model):
     type = models.CharField(max_length=50)  # Ação, FII, etc.
     ticker = models.CharField(max_length=10)
     current_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    pl = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    pvp = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    dividend_yeld = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         return f'{self.name} ({self.ticker})'
