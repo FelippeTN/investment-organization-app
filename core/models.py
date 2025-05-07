74ffb2cbdee2a590d03717426002fd939b53ae6d
FelippeTN <felippenalim2004@gmail.com>
2025-05-07 15:46:17 -0300

    FIX: models

--- core/models.py
+++ core/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Asset(models.Model):
-    name = models.CharField(max_length=100)
-    type = models.CharField(max_length=50)  # Ação, FII, etc.
-    ticker = models.CharField(max_length=10)
-    current_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    pl = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    pvp = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    dividend_yeld = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    ticker = models.CharField(max_length=20, unique=True)
+    type = models.CharField(max_length=50, blank=True, null=True)  # Ex.: Ação, FII
+    current_price = models.FloatField(default=0.0)
+    pl = models.FloatField(blank=True, null=True)  # P/L
+    pvp = models.FloatField(blank=True, null=True)  # P/VP
+    dividend_yield = models.FloatField(blank=True, null=True)  # Dividend Yield (%)
 
     def __str__(self):
         return f'{self.name} ({self.ticker})'
